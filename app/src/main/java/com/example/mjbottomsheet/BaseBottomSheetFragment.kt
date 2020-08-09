b39ad522e4ec46b6372b43058b4682c5769266b2
Minjae Kim <alswo5052@gmail.com>
2020-08-09 22:07:01 +0900

    Base bottom sheet 추가

---
+++ app/src/main/java/com/example/mjbottomsheet/BaseBottomSheetFragment.kt
@@ -0,0 +1,13 @@
+package com.example.mjbottomsheet
+
+import android.app.Dialog
+import android.os.Bundle
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+
+open class BaseBottomSheetFragment : BottomSheetDialogFragment() {
+    override fun getTheme(): Int = R.style.BottomSheetDialogTheme
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog = BottomSheetDialog(requireContext(), theme)
+}

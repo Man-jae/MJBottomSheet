bea1e0b1b13ac2db6c050c6aa1466bc701c00f53
Minjae Kim <alswo5052@gmail.com>
2020-08-09 22:07:57 +0900

    임시 Button 으로 bottom sheet 를 호출하도록 설정

--- app/src/main/java/com/example/mjbottomsheet/MainActivity.kt
+++ app/src/main/java/com/example/mjbottomsheet/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.mjbottomsheet
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.bottom_sheet.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_test.setOnClickListener {
+            BottomSheetFragment().apply {
+                show(supportFragmentManager, tag)
+            }
+        }
     }
 }

a0b380cb273269c40cb5ee28f985b3c1cf91195b
Minjae Kim <alswo5052@gmail.com>
2020-08-23 12:10:21 +0900

    Bottom sheet 설정 추가
    - 항상 최대 높이로 설정되도록 적용
    - 중간 높이로 닫히지 않도록 설정
    - 최대 높이가 match_parent 로 설정되도록 적용
    - 기타 레이아웃 수정

--- app/src/main/java/com/example/mjbottomsheet/BottomSheetFragment.kt
+++ app/src/main/java/com/example/mjbottomsheet/BottomSheetFragment.kt
@@ -1,12 +1,61 @@
 package com.example.mjbottomsheet
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewTreeObserver
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import kotlinx.android.synthetic.main.bottom_sheet.view.*
+
 
 open class BottomSheetFragment : BaseBottomSheetFragment() {
+    private lateinit var rootView: View
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.bottom_sheet, container, false)
+        rootView = inflater.inflate(R.layout.bottom_sheet, container, false)
+
+        setup()
+
+        return rootView
+    }
+
+    private fun setup() {
+        rootView.fl_btn_back.setOnClickListener { }
+        rootView.fl_btn_close.setOnClickListener { }
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val bottomSheetDialog = super.onCreateDialog(savedInstanceState) as BottomSheetDialog
+        bottomSheetDialog.setOnShowListener {
+            val bottomSheet: View = (it as BottomSheetDialog).findViewById(R.id.design_bottom_sheet)!!
+            BottomSheetBehavior.from(bottomSheet).state = BottomSheetBehavior.STATE_EXPANDED
+            BottomSheetBehavior.from(bottomSheet).skipCollapsed = true
+            BottomSheetBehavior.from(bottomSheet).isHideable = true
+        }
+        return bottomSheetDialog
+    }
+
+    override fun onStart() {
+        super.onStart()
+        dialog?.also {
+            val bottomSheet: View = (it as BottomSheetDialog).findViewById(R.id.design_bottom_sheet)!!
+            bottomSheet.layoutParams?.height = ViewGroup.LayoutParams.MATCH_PARENT
+            val behavior = BottomSheetBehavior.from(bottomSheet)
+            val layout = rootView.findViewById(R.id.bottom_sheet) as ConstraintLayout
+            layout.viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+                override fun onGlobalLayout() {
+                    try {
+                        layout.viewTreeObserver.removeGlobalOnLayoutListener(this)
+                        behavior.peekHeight = layout.height
+                        view?.requestLayout()
+                    } catch (e: Exception) {
+                    }
+                }
+            })
+        }
     }
 }
